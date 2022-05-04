8546a421bb4c930fd469328918a9352b30e9ece7
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-05-04 17:52:20 +0100

    Grid layout added

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,8 @@
-console.log('testing')
+function qs(node) {
+    return document.querySelector(node)
+}
+
+const arrowLeft = qs('.arrow-left')
+const arrowRight = qs('.arrow-right')
+const date = qs('.calender-date')
+const day = qs('.calender-day')
