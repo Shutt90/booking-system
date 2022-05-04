f92ce22a1a2438431f688d266c787f521098bb87
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-05-04 18:18:21 +0100

    Looped dates and returned value

--- frontend/main.js
+++ frontend/main.js
@@ -4,5 +4,29 @@ function qs(node) {
 
 const arrowLeft = qs('.arrow-left')
 const arrowRight = qs('.arrow-right')
-const date = qs('.calender-date')
+const calender = qs('.calender')
+const date = qs('.calender-dates')
 const day = qs('.calender-day')
+const arrival = qs('#form-arrival')
+const depart = qs('#form-depart')
+const dateChildren = date.querySelectorAll('div')
+
+function calenderState() {
+    if(calender.style.display === 'none') {
+        calender.style.display = 'flex'
+    } else {
+        calender.style.display = 'none'
+    }
+}
+
+function getValue(e) {
+    console.log(e.target.textContent.trim())
+}
+
+depart.addEventListener('click', calenderState)
+arrival.addEventListener('click', calenderState)
+
+
+dateChildren.forEach(date => {
+    date.addEventListener('click', getValue)
+})
