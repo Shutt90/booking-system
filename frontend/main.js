5a4ae43c8525e06a9ae9d3c78be2f556d0ef7410
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-05-06 20:41:31 +0100

    Working towards calender input on click

--- frontend/main.js
+++ frontend/main.js
@@ -10,22 +10,48 @@ const day = qs('.calender-day')
 const arrival = qs('#form-arrival')
 const depart = qs('#form-depart')
 const dateChildren = date.querySelectorAll('div')
+let activeForm = qs('.active')
+
+const options = [qs('#form-arrival'), qs('#form-depart')]
 
 function calenderState() {
-    if(calender.style.display === 'none') {
+
+    // options.forEach((node, index) => {
+        
+    // })
+
+    this.addEventListener('click', function() {
+        this.classList.toggle('active');
+    })
+
+    if(this.classList.contains('active') && calender.style.display === 'flex') {
+        this.classList.remove('active')
+    } else if(!this.classList.contains('active')) {
+        this.classList.add('active')
+    }
+
+    const activeFormNew = qs('.active')
+    console.log(activeFormNew)
+
+    if(activeFormNew.classList.contains('active')) {
         calender.style.display = 'flex'
-    } else {
+    } else if(!activeFormNew === null){
         calender.style.display = 'none'
     }
+
 }
 
 function getValue(e) {
-    console.log(e.target.textContent.trim())
+    const selected = e.target.textContent.trim()
+    if(depart.classList.contains('active')) {
+        depart.value = selected 
+    } else if (arrival.classList.contains('active')) {
+        arrival.value = selected
+    }
 }
 
-depart.addEventListener('click', calenderState)
+depart.addEventListener('click', calenderState, this)
 arrival.addEventListener('click', calenderState)
-
 
 dateChildren.forEach(date => {
     date.addEventListener('click', getValue)
