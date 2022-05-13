c3934c3ebf2e00ca3ed072eee744f2981df55259
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-05-13 17:52:31 +0100

    Correctly fileserve, not equiptwith mux yet

--- main.go
+++ main.go
@@ -15,11 +15,13 @@ var db *gorm.DB
 
 func main() {
 
+	fs := http.FileServer(http.Dir("./frontend/"))
+
 	r := mux.NewRouter()
 	routes.RegisterBookingsRoutes(r)
 	http.Handle("/", r)
 	fmt.Printf("Starting server at port 8080\n")
-	log.Fatal(http.ListenAndServe("localhost:8080", r))
+	log.Fatal(http.ListenAndServe("localhost:8080", fs))
 
 	db = config.GetDB()
 
