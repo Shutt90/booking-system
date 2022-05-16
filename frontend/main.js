568654b3ec9f7fa00eb57e60731a7db1c51e132a
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-05-16 20:33:54 +0100

    Finally worked out the active algo!!!

--- frontend/main.js
+++ frontend/main.js
@@ -1,47 +1,47 @@
 function qs(node) {
-    return document.querySelector(node)
+  return document.querySelector(node);
 }
 
-const arrowLeft = qs('.arrow-left')
-const arrowRight = qs('.arrow-right')
-const calender = qs('.calender')
-const date = qs('.calender-dates')
-const day = qs('.calender-day')
-const arrival = qs('#form-arrival')
-const depart = qs('#form-depart')
-const dateChildren = date.querySelectorAll('div')
-let activeForm = qs('.active')
+const arrowLeft = qs('.arrow-left');
+const arrowRight = qs('.arrow-right');
+const calender = qs('.calender');
+const date = qs('.calender-dates');
+const day = qs('.calender-day');
+const arrival = qs('#form-arrival');
+const depart = qs('#form-depart');
+const dateChildren = date.querySelectorAll('div');
+let activeForm = qs('.active');
 
-const options = [qs('#form-arrival'), qs('#form-depart')]
-
-let calenderOpen = false;
+const options = [qs('#form-arrival'), qs('#form-depart')];
 
 function calenderState() {
+  const active = qs('.active');
 
-    if(calenderOpen && this.classList.contains('active')) {
-        this.classList.remove('active')
-        calender.style.display = 'none';
-        calenderOpen = false;
-    } else if(!calenderOpen && !this.classList.contains('active')) {
-        this.classList.add('active');
-        calender.style.display = 'flex';
-        calenderOpen = true;
-
-    }
+  if (this.classList.contains('active')) {
+    this.classList.remove('active');
+    calender.style.display = 'none';
+  } else if (active) {
+    active.classList.remove('active');
+    calender.style.display = 'flex';
+    this.classList.add('active');
+  } else {
+    this.classList.add('active');
+    calender.style.display = 'flex';
+  }
 }
 
 function getValue(e) {
-    const selected = e.target.textContent.trim()
-    if(depart.classList.contains('active')) {
-        depart.value = selected 
-    } else if (arrival.classList.contains('active')) {
-        arrival.value = selected
-    }
+  const selected = e.target.textContent.trim();
+  if (depart.classList.contains('active')) {
+    depart.value = selected;
+  } else if (arrival.classList.contains('active')) {
+    arrival.value = selected;
+  }
 }
 
-depart.addEventListener('click', calenderState, this)
-arrival.addEventListener('click', calenderState, this)
+depart.addEventListener('click', calenderState, this);
+arrival.addEventListener('click', calenderState, this);
 
-dateChildren.forEach(date => {
-    date.addEventListener('click', getValue)
-})
+dateChildren.forEach((date) => {
+  date.addEventListener('click', getValue);
+});
